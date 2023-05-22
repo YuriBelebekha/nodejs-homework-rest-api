aa945d3da1a04035f87a19c0e5d6d18f3ec27258
Yuri Belebekha <ybelebekha@gmail.com>
2023-05-23 01:25:46 +0300

    Fixed some code

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -18,7 +18,7 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {  
-  const result = await contacts.addContact(req.body);
+  const result = await contacts.addContact(req.body); 
   res.status(201).json(result);  
 };
 
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,10 +1,20 @@
 const Joi = require('joi');
 
 const contactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+  name: Joi.string()
+    .min(2)
+    .max(15)
+    .required(),
+  email: Joi.string()
+    .min(2)
+    .max(20)
+    .required(),
+  phone: Joi.string()
+    .required(),
+})
+  .messages({
+    'any.required': 'missing required {{#label}} field',
+  });
 
 module.exports = {
   contactSchema,
