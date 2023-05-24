370a6d248ba19eaa2e1f9753e62c34a74020c993
Yuri Belebekha <ybelebekha@gmail.com>
2023-05-24 23:00:27 +0300

    Added mongoose and DB_HOST

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app');
+
+const DB_HOST = "mongodb+srv://ybelebekha:55XB2Hi65Z8XXYfP@cluster0.qm59hnr.mongodb.net/db_contacts?retryWrites=true&w=majority";
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
