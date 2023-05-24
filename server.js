1d86e2b0fb6b38617a0dbc901ffe09cd22de8b7d
Yuri Belebekha <ybelebekha@gmail.com>
2023-05-25 00:18:55 +0300

    Added .env and dotenv

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const DB_HOST = "mongodb+srv://ybelebekha:55XB2Hi65Z8XXYfP@cluster0.qm59hnr.mongodb.net/db_contacts?retryWrites=true&w=majority";
+const {DB_HOST, PORT = 3000} = process.env;
 
 mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch(error => {
     console.log(error.message);
