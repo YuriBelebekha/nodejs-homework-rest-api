db7aa259b055c8982c6ec14b0afb527f88b23c47
Yuri Belebekha <ybelebekha@gmail.com>
2023-06-06 23:50:40 +0300

    Added token generation

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const { register } = require('../../controllers/auth');
+const ctrl = require('../../controllers/auth');
 const { validateBody } = require('../../middlewares');
 const { schemas } = require('../../models/user');
 
 const router = express.Router();
 
-router.post('/register', validateBody(schemas.registerSchema), register); //
+router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
+router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
 
 module.exports = router;
