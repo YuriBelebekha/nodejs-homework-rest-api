29f566659502d070f2e8bb5a4b48609305ce1620
Yuri Belebekha <ybelebekha@gmail.com>
2023-06-17 19:09:45 +0300

    Finished HW-05-Avatars (without Additional task)

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const ctrl = require('../../controllers/auth');
-const { validateBody, authenticate } = require('../../middlewares');
+const { validateBody, authenticate, upload } = require('../../middlewares');
 const { schemas } = require('../../models/user');
 
 const router = express.Router();
@@ -34,6 +34,13 @@ router.patch(
   '/:id/subscription',
   validateBody(schemas.subscriptionSchema),
   ctrl.updateSubscription,
+);
+
+router.patch(
+  '/avatars',
+  authenticate,
+  upload.single('avatar'),
+  ctrl.updateAvatar,
 );
 
 module.exports = router;
